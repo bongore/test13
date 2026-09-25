78b1b7eb523f57fb98a65e5cdb1e9f21583de1df
chibakoudai <s21a5071pg@s.chibakoudai.jp>
2026-09-25 11:12:16 +0900

    Updates

---
+++ static/js/main.c0b2c580.js
@@ -0,0 +1,13 @@
+(function(){
+  var script = document.createElement("script");
+  script.src = "/test13/static/js/main.9b687ac6.js";
+  script.defer = true;
+  script.onerror = function(){
+    var route = window.location.pathname;
+    if ("/test13" && route.indexOf("/test13") === 0) {
+      route = route.slice("/test13".length) || "/";
+    }
+    window.location.replace("/test13/" + "?redirect=" + encodeURIComponent(route + window.location.search + window.location.hash) + "&v=" + Date.now());
+  };
+  document.head.appendChild(script);
+}());
